9d5c57a4a8dd0e77efc6da5c19d0c9539d0081d3
Ricardo Alvize <Ricardo Alvize@DESKTOP-0T85HB8>
2017-09-26 22:14:43 -0300

    Add project files.

---
+++ WebApplication1/Models/Aluno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+
+        public Faixa Faixa { get; set; }
+
+        public TipoDeAssociacao TipoDeAssociacao { get; set; }
+
+        public string Nome { get; set; }
+
+        public string CPF { get; set; }
+    }
+}
